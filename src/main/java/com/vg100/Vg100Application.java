463db8f15dfd314dcf06b0c7487e575257944a5f
Matt Cadorette <mcadorette@gmail.com>
2018-11-29 14:27:49 -0500

    Initial commit from Atomist

---
+++ src/main/java/com/vg100/Vg100Application.java
@@ -0,0 +1,12 @@
+package com.vg100;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Vg100Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Vg100Application.class, args);
+	}
+}
